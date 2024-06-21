e34735e1e4d6c0eb40906bc59893ac129a149b25
Lehlomela Mokoena <lehlomela.mokoena@momentum.co.za>
2024-06-21 16:53:59 +0200

    initial commit

---
+++ src/main/java/za/co/lehlomela/me_kasi/MeKasiApplication.java
@@ -0,0 +1,13 @@
+package za.co.lehlomela.me_kasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeKasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeKasiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/lehlomela/me_kasi/MeKasiApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.lehlomela.me_kasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeKasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
