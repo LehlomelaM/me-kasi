d067c1afea20615e43d6b547f607733111eb97a5
Lehlomela Mokoena <lehlomela.mokoena@momentum.co.za>
2024-07-07 17:47:03 +0200

    configured datasource

---
+++ src/main/java/za/co/lehlomela/me_kasi/config/DatasourceConfiguration.java
@@ -0,0 +1,18 @@
+package za.co.lehlomela.me_kasi.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatasourceConfiguration {
+
+    @Bean
+    @ConfigurationProperties(prefix = "app.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
