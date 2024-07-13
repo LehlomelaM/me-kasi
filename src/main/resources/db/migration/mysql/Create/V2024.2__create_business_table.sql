78f7faa36e6e6db6d440de7a168ea3c79725d37a
Lehlomela Mokoena <lehlomela.mokoena@momentum.co.za>
2024-07-13 16:37:46 +0200

    sql: create business table

---
+++ src/main/resources/db/migration/mysql/Create/V2024.2__create_business_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Businesses
+(
+    id           BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name         VARCHAR(150) NOT NULL,
+    phone_number VARCHAR(20),
+    email        VARCHAR(100) NOT NULL UNIQUE,
+    website      VARCHAR(200) UNIQUE,
+    description  TEXT,
+    profile_img_url  VARCHAR(200)
+);
