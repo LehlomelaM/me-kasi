8b966d7205c110393ce852e8e55e206eed4b8623
Lehlomela Mokoena <lehlomela.mokoena@momentum.co.za>
2024-07-13 20:02:05 +0200

    config update: enable jdbc repositories

--- src/main/java/za/co/lehlomela/me_kasi/config/DatasourceConfiguration.java
+++ src/main/java/za/co/lehlomela/me_kasi/config/DatasourceConfiguration.java
@@ -3,12 +3,23 @@ package za.co.lehlomela.me_kasi.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import za.co.lehlomela.me_kasi.common.Repository.BaseRepositoryJdbc;
 
 import javax.sql.DataSource;
 
 @Configuration
-public class DatasourceConfiguration {
+@EnableJdbcRepositories( /*activate spring SDJ repositories*/
+        includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {BaseRepositoryJdbc.class})
+//        queryLookupStrategy = QueryLookupStrategy.Key.USE_DECLARED_QUERY
+//        repositoryBaseClass = BaseRepositoryJdbc.class
+)
+public class DatasourceConfiguration
+        extends AbstractJdbcConfiguration /*provides default beans required SDJ*/ {
 
     @Bean
     @ConfigurationProperties(prefix = "app.datasource")
