718973d29861fcd970abb206c5860d2f7ac3e596
Lehlomela Mokoena <lehlomela.mokoena@momentum.co.za>
2024-07-14 14:35:26 +0200

    Address class moved

--- src/main/java/za/co/lehlomela/me_kasi/common/Address.java
+++
@@ -1,8 +0,0 @@
-package za.co.lehlomela.me_kasi.common;
-
-public class Address {
-    private String street;
-    private String city;
-    private Province province;
-    private String zipCode;
-}
