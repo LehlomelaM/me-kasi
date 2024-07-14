36e26a72ac7a144711dba689886d21314bd68504
Lehlomela Mokoena <lehlomela.mokoena@momentum.co.za>
2024-07-14 14:34:31 +0200

    new: review class

---
+++ src/main/java/za/co/lehlomela/me_kasi/business/Review.java
@@ -0,0 +1,9 @@
+package za.co.lehlomela.me_kasi.business;
+
+
+public class Review {
+    private String id;
+    private String businessId;
+    private String reviewText;
+    private int rating;
+}
