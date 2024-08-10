a50c7b317466d7cb0a42cfa76031c35ab7c19096
Lehlomela Mokoena <lehlomela.mokoena@momentum.co.za>
2024-08-10 19:31:41 +0200

    add paging and sorting for business

--- src/main/java/za/co/lehlomela/me_kasi/business/BusinessController.java
+++ src/main/java/za/co/lehlomela/me_kasi/business/BusinessController.java
@@ -1,11 +1,17 @@
 package za.co.lehlomela.me_kasi.business;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
 @RestController()
@@ -37,5 +43,15 @@ public class BusinessController {
                 .toUri();
 
         return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Business>> findAll(Pageable pageable) {
+        PageRequest pageRequest = PageRequest.of(pageable.getPageNumber(),
+                pageable.getPageSize(),
+                pageable.getSortOr(Sort.by(Sort.Direction.ASC, "id")));
+
+        Page<Business> page = businessRepository.findAll(pageRequest);
+        return ResponseEntity.ok(page.getContent());
     }
 }
--- src/main/java/za/co/lehlomela/me_kasi/business/BusinessRepository.java
+++ src/main/java/za/co/lehlomela/me_kasi/business/BusinessRepository.java
@@ -1,9 +1,11 @@
 package za.co.lehlomela.me_kasi.business;
 
+import org.springframework.data.repository.ListPagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import za.co.lehlomela.me_kasi.common.Repository.BaseRepositoryJdbc;
 
 @Repository
-public interface BusinessRepository extends BaseRepositoryJdbc<Business, Long> {
+public interface BusinessRepository extends BaseRepositoryJdbc<Business, Long>,
+        ListPagingAndSortingRepository<Business, Long> {
 
 }
